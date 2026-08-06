59e0a2753113a109af2c10168fb85ffda3da6f3a
lasandisal <lasandisalwathura@gmail.com>
2026-08-06 13:12:32 +0530

    spring initialized

---
+++ src/main/java/lk/ijse/eventsphere/EventsphereApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.eventsphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsphereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ijse/eventsphere/EventsphereApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.eventsphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
